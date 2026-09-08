b9636ca84588720f4cbd40823897fff94a5f81c8
ddasik76-cmd <ddasik76@gmail.com>
2026-09-08 17:48:26 +0900

    지역검색 업데이트 캐시 갱신

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "school-garden-v15";
+const CACHE = "school-garden-v16";
 const BASE = new URL("./", self.location.href).pathname;
 const CORE = [BASE, BASE + "index.html", BASE + "manifest.webmanifest", BASE + "mobile-fixes.css?v=22", BASE + "garden-location-search.js?v=1"];
 
